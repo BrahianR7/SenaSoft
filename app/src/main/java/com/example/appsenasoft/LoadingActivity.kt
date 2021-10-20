ca0ef28af80afed6370e9690e5e5fbddd446a510
julianAG <julianagrg4l@gmail.com>
2021-10-20 09:48:37 -0500

    splash

--- app/src/main/java/com/example/appsenasoft/LoadingActivity.kt
+++ app/src/main/java/com/example/appsenasoft/LoadingActivity.kt
@@ -1,11 +1,21 @@
 package com.example.appsenasoft
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class LoadingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loading)
+
+    }
+
+
+    fun initAp (view :View){
+
+        intent = Intent( this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
