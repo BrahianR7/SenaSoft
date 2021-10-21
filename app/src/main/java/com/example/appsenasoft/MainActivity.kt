3ef935e009e0e1226c514230034b5594ab22a84e
julianAG <julianagrg4l@gmail.com>
2021-10-21 14:50:18 -0500

    splash

--- app/src/main/java/com/example/appsenasoft/MainActivity.kt
+++ app/src/main/java/com/example/appsenasoft/MainActivity.kt
@@ -4,49 +4,81 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
-class   MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener  {
+class   MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var drawerlayout: DrawerLayout
+    private lateinit var drawer: DrawerLayout
     private lateinit var toogle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
 
         Thread.sleep(2000)
-                    super.onCreate(savedInstanceState)
-                    setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        toogle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toogle)
 
 
 
-        toogle = ActionBarDrawerToggle(this,drawerlayout,R.string.open_drawer,R.string.close_drawer)
-        drawerlayout.addDrawerListener(toogle)
-        toogle.syncState()
+
+
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
 
-        nav_view.setNavigationItemSelectedListener{
-            when( it.itemId){
-                R.id.nav_home -> {
-                    supportFragmentManager.beginTransaction().apply {
-                        replace(R.id.fragmentContainerView, HomeFragment())
-                        commit()
-                    }
-                }
-            }
-        }
-    }
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toogle.onOptionsItemSelected(item)){
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
 
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        TODO("Not yet implemented")
-    }
 
 }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> Toast.makeText(this, "Registro",Toast.LENGTH_SHORT).show()
+            R.id.nav_exit -> Toast.makeText(this, "Salida",Toast.LENGTH_SHORT).show()
+
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    
+
+    }
+//nav_view.setNavigationItemSelectedListener {
+//     when(listOf(it)){
+// R.id.nav_home -> {
+//    supportFragmentManager.beginTransaction().apply {
+//         replace(R.id.fragmentContainerView, HomeFragment())
+//           commit()
+//         }
+//       }
+//     }
+//   }
+// }
+
+// override fun onOptionsItemSelected(item: MenuItem): Boolean {
+// if (toogle.onOptionsItemSelected(item)){
+//   return true
+//}
+//  return super.onOptionsItemSelected(item)
+//}
